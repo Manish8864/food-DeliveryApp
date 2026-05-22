7d1dc482a3ae2974f49eb1332f0af74b13ffb16b
Manish8864 <manish0kumar42@gmail.com>
2026-05-22 16:04:45 +0530

    Block background scroll when login popup is open

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,20 +19,21 @@ const App = () => {
 
   return (
     <>
-    {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
-      <div className='app'>
-      <Navbar setShowLogin={setShowLogin} />
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/cart' element={<Cart/>} />
-        <Route path='/order' element={<PlaceOrder />} />
-        <Route path='/verify' element = {<Verify/>} />
-        <Route path='/MyOrders' element = {<MyOrders/>} />
-      </Routes>
-    </div>
-    <Footer/>
+      {showLogin ? <LoginPopup setShowLogin={setShowLogin} /> : <></>}
+      <div className={showLogin ? 'no-scroll' : ''}>
+        <div className='app'>
+          <Navbar setShowLogin={setShowLogin} />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/cart' element={<Cart />} />
+            <Route path='/order' element={<PlaceOrder />} />
+            <Route path='/verify' element={<Verify />} />
+            <Route path='/MyOrders' element={<MyOrders />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
     </>
-    
   )
 }
 
